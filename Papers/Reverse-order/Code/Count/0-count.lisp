28fced8b1bb394ca8676638bb2cda375fb449831
Martin Cracauer <cracauer@cons.org>
2019-09-11 14:58:24 -0400

    wavehh.hanse.de x86_64 Linux 5.0.13-cracauerdlwavehh Debian GNU/Linux 10 \n \l

--- Papers/Reverse-order/Code/Count/0-count.lisp
+++
@@ -1,3 +0,0 @@
-;; standard version
-(defun reverse-count-0 (x list)
-  (count x list :from-end t :test #'eq))
--- Papers/Reverse-order/Code/Count/1-count.lisp
+++
@@ -1,6 +0,0 @@
-(defun reverse-count-1 (x list)
-  (declare (optimize
-            (speed 3) (debug 0) (safety 0)
-            (compilation-speed 0)))
-  (loop for e in (reverse list)
-        count (eq x e)))
